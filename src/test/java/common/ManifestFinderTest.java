3055189435a737a87da899befa830a7154fad437
MiaouZart <miaouzart@gmail.com>
2025-04-16 18:08:01 +0200

    Fix ManifestFinderTest.java

--- src/test/java/common/ManifestFinderTest.java
+++ src/test/java/common/ManifestFinderTest.java
@@ -1,22 +1,23 @@
 package common;
 
+import org.junit.jupiter.api.Test;
 import java.io.IOException;
 
 import static common.manifestFinder.findManifest;
 import static org.junit.jupiter.api.Assertions.*;
 
-class manifestFinderTest {
-
+public class ManifestFinderTest {
+    @Test
     void test1_21_5() throws IOException {
-        String url1_21_5 = ""; // Valeur attendue
+        String url1_21_5 = "";
         String urlcreated = findManifest("1.21.5");
         assertEquals(url1_21_5, urlcreated);
     }
 
+    @Test
     void testNotFind() {
         assertThrows(IllegalArgumentException.class, () -> {
             findManifest("2.0.0");
         });
-
     }
 }
